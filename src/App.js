0422a7cc25da468a7d4bf1e1f70b2e330dbc09ac
Ian Peter Greenburg <email": null>
2017-05-10 15:57:28 -0700

    creating a basic component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>React Profile Workshop</h2>
         </div>
+
         <p className="App-intro">
           This is the starting point to build your app on top of!
         </p>
+
       </div>
     );
   }
---
+++ src/components/BasicComponent.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class BasicComponent extends Component {
+  render(){
+    return(<div>
+        Hi, I'm a Basic Component!
+      </div>)
+  }
+}
