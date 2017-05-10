f43ff7fd83232678b56c395ed2d7669dff7af610
Ian Peter Greenburg <email": null>
2017-05-10 15:56:53 -0700

    establishing starting point for app

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Profile Workshop</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is the starting point to build your app on top of!
         </p>
       </div>
     );
