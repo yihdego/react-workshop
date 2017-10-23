aeb26592baf002f030bc85c5ebe97568b88f8f03
Amanuel <yihdego@msn.com>
2017-10-23 16:26:39 -0700

    padding to nav-bar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -9,8 +9,8 @@ class Navbar extends Component {
           <div className='container'>
             <ul className='list-inline nav-left nav-list'>
               <li onClick={this.props.onClick} id='home'>Home</li>
-              <li onClick={this.props.onClick} id='about'>About</li>
-              <li onClick={this.props.onClick} id='contact'>Contact</li>
+              <li onClick={this.props.onClick} id='about'>Signature Moves</li>
+              <li onClick={this.props.onClick} id='contact'>Promotional Images</li>
             </ul>
             <div className='brand'>
               <img src={logo} className="App-logo" alt="logo" />
